a7423e3696d4232d86bd135cd858e17080fe74eb
Tejal Gupta <48616328+tejal-gupta@users.noreply.github.com>
2020-09-02 16:58:40 +0530

    Add files via upload

---
+++ Program 3- Loops and branches combine/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Program_3__Loops_and_branches_combine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            Console.WriteLine($"The sum of numbers divisible by 3 is {sum}");
+        }
+    }
+}
